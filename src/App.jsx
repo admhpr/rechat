6f7212c83eca877a7f71920497b773e9d7b2302b
Adam Harpur <adam@harpur.io>
2019-01-07 16:51:50 -0600

    added chatkit keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Chatkit from "@pusher/chatkit";
 import MessageList from "./components/MessageList";
 import SendMessageForm from "./components/SendMessageForm";
 import RoomList from "./components/RoomList";
 import NewRoomForm from "./components/NewRoomForm";
 
+import { instanceLocator } from "./config";
 class App extends React.Component {
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator
+    });
+  }
   render() {
     return (
       <div className="app">
