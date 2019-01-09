ec5de39a0cb3feba81e83c921353ea5318c157b3
Adam Harpur <adam@harpur.io>
2019-01-09 13:27:48 -0600

    send message form

--- src/components/SendMessageForm.jsx
+++ src/components/SendMessageForm.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { throws } from "assert";
 
 class SendMessageForm extends React.Component {
+  constructor() {
+    super();
+    this.state = { message: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ message: e.target.value });
+  }
   render() {
     return (
       <form className="send-message-form">
-        <input placeholder="SendMessageForm" type="text" />
+        <input
+          value={this.state.message}
+          onChange={this.handleChange}
+          placeholder="Type your message here"
+          type="text"
+        />
       </form>
     );
   }
