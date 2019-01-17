8f7e96812c9ce91364398edf1253f5a9b73d222a
Adam Harpur <adam@harpur.io>
2019-01-17 13:32:17 -0600

    style: color changes

--- src/components/RoomList.jsx
+++ src/components/RoomList.jsx
@@ -6,7 +6,7 @@ class RoomList extends React.Component {
     return (
       <div className="rooms-list">
         <ul>
-          <h3>Your rooms:</h3>
+          <h3 className="room-list-header">Your rooms:</h3>
           {orderedRooms.map(room => {
             const active = this.props.roomId === room.id ? "active" : "";
             return (
